7045e19bb4c0b348304d1f3516f2ef72f60eb9ee
flrser <113252009+flrser@users.noreply.github.com>
2022-09-16 11:15:53 +0700

    Add files via upload

---
+++ floris.py
@@ -0,0 +1,54 @@
+#belajar elemen dasar python
+print("hello yak")
+
+bendaRingan ="hp"
+
+print("saya mempunyai benda bernama"+bendaRingan)
+
+#type data di python:1.string 2.int 3.float 4.boolean 
+
+#string
+
+string = "ini type data string"
+print(string)
+
+#int
+int = 10
+print("ini interger",+int)
+
+#tipe data float
+float = 10,5
+
+#boolean 
+#false dan true
+print("tipe data boolean=",+10 > 9)
+
+a=44
+b=15
+
+print("termasuk tipe data boolean apa?",+a==b)
+
+#operator aritmatika
+# (+),(-),(),(/),(*),(%)
+
+#penjumlahan
+a = 5
+b = 2
+print("hasil dari penjumlahan adalah =",+a+b)
+
+#pengurangan
+print("hasil dari pengurangan adalah =",+a-b)
+
+#perkalian
+print("hasil dari perkalian adalah =",+a*b)
+
+#pembagian
+print("hasil dari pembagian adalah =",+a/b)
+
+#kuadrat
+print("hasil dari kuadrat adalah =",+a**b)
+
+#modulus
+a=7
+b=2
+print("hasil dari modulus adalah =",+a%b)
